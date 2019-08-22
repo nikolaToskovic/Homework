8140dcf041fa3706daee1be58d7511d1abdb9cfa
nikolToskovic <nikolatoskovic1@gmail.com>
2019-08-22 17:41:52 +0200

    Homework new

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, {Component, Fragment} from 'react';
+import './App.css';
+import {Header} from './components/Header/Header'; 
+import data from './data/data.json';
+import {Cards} from './components/Cards/Cards';
+
+class App extends Component {
+
+  state = {
+      showCards: true,
+      data: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      data: data
+    })
+  }
+
+  onCardRemove = (e) => {
+    console.log(e.target)
+  }
+
+  renderCards = () => {
+
+    const displayData = this.state.searchedData || this.state.data;
+
+    return this.state.showCards && this.state.data.length ? <Cards data={displayData} removeCard={this.onCardRemove}/> : null;
+  }
+
+  onCardSearch = (data) => {
+    this.setState({
+      searchedData: data
+    })
+  }
+
+  render () {
+    return (
+      <Fragment>
+        <Header className="full-width" 
+                data={this.state.data}
+                onSearch={(data) => this.onCardSearch(data) }
+                />
+        <div className="main-content" onClick={this.update}>
+             {this.renderCards()}
+        </div>
+      </Fragment>
+    );
+  }
+  
+}
+
+export default App;
